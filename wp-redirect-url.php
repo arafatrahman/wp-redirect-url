41c912b630044fea118443067912e7af49aab088
Arafat Rahman <70848865+kauniaweb@users.noreply.github.com>
2020-12-11 00:01:31 +0600

    invalid argument foreach

--- wp-redirect-url.php
+++ wp-redirect-url.php
@@ -81,14 +81,15 @@ if (!function_exists('kaupost')) {
 
 }
 
-
 function recursive_sanitize_text_field($array) {
-
-    foreach ($array as $key => &$value) {
-        if (is_array($value)) {
-            $value = recursive_sanitize_text_field($value);
-        } else {
-            $value = sanitize_text_field($value);
+    
+    if ($array) {
+        foreach ($array as $key => &$value) {
+            if (is_array($value)) {
+                $value = recursive_sanitize_text_field($value);
+            } else {
+                $value = sanitize_text_field($value);
+            }
         }
     }
 
